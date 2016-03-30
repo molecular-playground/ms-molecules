cfa3839f4bcc42888976e89be13f09012d25c9a4
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-29 20:26:25 -0400

    Started filling out request all molecules url route

--- routes/molecules.js
+++ routes/molecules.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db.js');
 
 // edit user
 router.get('/', function(req, res, next){
-  var name = req.body.name;
-  if(name){
-    res.send({success: true, message: "You just made a successful request!"});
-  }
+  var query = "SELECT name, link FROM molecules";
+  db.query({text: query}. function (err, results) {
+    if (err) {
+      next(err);
+      return;
+    }
+    
+  });
 });
 
+router.post('/', auth, function(req, res, next))
 module.exports = router;
