0750a8d2c4e8e37ed6c052de09fb9ed5395ce735
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-29 20:02:50 -0400

    added more stuff

--- routes/molecules.js
+++ routes/molecules.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 // edit user
-router.get('/', auth, function(req, res, next){
+router.get('/', function(req, res, next){
   var name = req.body.name;
   if(name){
     res.send({success: true, message: "You just made a successful request!"});
   }
 });
+
+module.exports = router;
