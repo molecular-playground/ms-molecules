979d7e7d3a12f4fdc64e57d3a8f66f229aaa209a
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-31 19:10:39 -0400

    Properly returns names and links for each molecule in the database as an array of JSON objects

--- routes/molecules.js
+++ routes/molecules.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/db.js');
+var MS_HOSTING_URL = 'http://mshosting:3000/';
 
 // edit user
 router.get('/', function(req, res, next){
@@ -11,7 +12,13 @@ router.get('/', function(req, res, next){
       return;
     }
     else {
-      res.send({success: true, message: results.rows});
+      var moleculeEntries = results.rows;
+      var moleculesWithURL = []
+      for (molecule of moleculeEntries) {
+        molecule.link = MS_HOSTING_URL + molecule.link;
+        moleculesWithURL.push(molecule);
+      }
+      res.send({success: true, message: moleculesWithURL});
     }
   });
 });
