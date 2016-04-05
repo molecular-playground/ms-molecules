62649064ba7a8639f4880a19747cbe5fe266e11b
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-04-05 19:37:52 -0400

    Added a route that gets a molecules that match a name from the database and returns them

--- routes/molecules.js
+++ routes/molecules.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var db = require('../lib/db.js');
 var MS_HOSTING_URL = 'http://mshosting:3000/';
 
-// edit user
-router.get('/', function(req, res, next){
+// get the list of all molecules
+router.get('/', function(req, res, next) {
   var query = "SELECT mid, name, link FROM Molecules";
   db.query({text: query}, function (err, results) {
     if (err) {
@@ -25,6 +25,25 @@ router.get('/', function(req, res, next){
       }
       res.send({success: true, message: moleculesWithURL});
     }
+  });
+});
+// Gets the molecules from the database
+// whose name is "like" the one passed to this route.
+router.get('/:name', function(req, res, next) {
+  var name = req.params.name;
+  var query = 'SELECT * FROM molecules WHERE name LIKE '$1'';
+  db.query({text: query, values: ['%name%']}, function(err, results) {
+    if (err) {
+      next(err);
+      return;
+    }
+    // Return the molecules that have matched.
+    var molecules = [];
+    for (molecule of results.rows) {
+      molecule.link = MS_HOSTING_URL + molecule.link;
+      molecules.push(molecule);
+    }
+    res.send({success: true, message: molecules});
   });
 });
 
